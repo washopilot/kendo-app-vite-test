cb12b2158c6bd8c5807cb25536c5e74a547a5995
Fernando Chicaiza <washopilot@yahoo.com>
2023-09-08 11:41:22 -0500

    first try licencing kendo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { State, process } from '@progress/kendo-data-query';
+import { Window, WindowActionsEvent } from '@progress/kendo-react-dialogs';
+import { DropDownList, DropDownListChangeEvent } from '@progress/kendo-react-dropdowns';
+import { Grid, GridColumn, GridDataStateChangeEvent, GridRowClickEvent } from '@progress/kendo-react-grid';
+import '@progress/kendo-theme-default/dist/all.css';
+import { useEffect, useState } from 'react';
+import categories from './categories.json';
+import products from './products.json';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface AppState {
+    dropdownlistCategory: string;
+    gridDataState: State;
+    windowVisible: boolean;
+    gridClickedRow: any;
 }
 
-export default App
+const initState: AppState = {
+    dropdownlistCategory: '',
+    gridDataState: {
+        sort: [{ field: 'ProductName', dir: 'asc' }],
+        skip: 0,
+        take: 10
+    },
+    windowVisible: false,
+    gridClickedRow: {}
+};
+
+const App = () => {
+    const [state, setState] = useState<AppState>(initState);
+
+    const handleDropDownChange = (e: DropDownListChangeEvent) => {
+        const newDataState = { ...state.gridDataState };
+        if (e.target.value.CategoryID !== null) {
+            newDataState.filter = {
+                logic: 'and',
+                filters: [{ field: 'CategoryID', operator: 'eq', value: e.target.value.CategoryID }]
+            };
+            newDataState.skip = 0;
+        } else {
+            newDataState.filter = undefined;
+            newDataState.skip = 0;
+        }
+        setState({ ...state, dropdownlistCategory: e.target.value.CategoryID, gridDataState: newDataState });
+    };
+
+    useEffect(() => console.log({ state }), [state]);
+
+    const handleGridDataStateChange = (e: GridDataStateChangeEvent) => {
+        setState({ ...state, gridDataState: e.dataState });
+    };
+
+    const handleGridRowClick = (e: GridRowClickEvent) => {
+        setState({ ...state, windowVisible: true, gridClickedRow: e.dataItem });
+    };
+
+    const closeWindow = (e: WindowActionsEvent) => {
+        setState({ ...state, windowVisible: false });
+    };
+
+    const checkboxColumn = (props: any) => {
+        return (
+            <td>
+                <input type='checkbox' checked={props.dataItem[props.field]} disabled={true} />
+            </td>
+        );
+    };
+
+    return (
+        <div className='App'>
+            <h1>Hello KendoReact!</h1>
+            <p>
+                <DropDownList
+                    data={categories}
+                    dataItemKey='CategoryID'
+                    textField='CategoryName'
+                    defaultItem={{ CategoryID: null, CategoryName: 'Product categories' }}
+                    onChange={handleDropDownChange}
+                />
+                &nbsp; Selected category ID: <strong>{state.dropdownlistCategory}</strong>
+            </p>
+            <Grid
+                data={process(products, state.gridDataState)}
+                pageable={true}
+                sortable={true}
+                {...state.gridDataState}
+                onDataStateChange={handleGridDataStateChange}
+                style={{ height: '400px' }}
+                onRowClick={handleGridRowClick}>
+                <GridColumn field='ProductName' title='Product Name' />
+                <GridColumn field='UnitPrice' title='Price' format='{0:c}' />
+                <GridColumn field='UnitsInStock' title='Units in Stock' />
+                <GridColumn field='Discontinued' cell={checkboxColumn} />
+            </Grid>
+
+            {state.windowVisible && (
+                <Window title='Product Details' onClose={closeWindow} height={250}>
+                    <dl>
+                        <dt>Product Name</dt>
+                        <dd>{state.gridClickedRow.ProductName}</dd>
+                        <dt>Product ID</dt>
+                        <dd>{state.gridClickedRow.ProductID}</dd>
+                        <dt>Quantity per Unit</dt>
+                        <dd>{state.gridClickedRow.QuantityPerUnit}</dd>
+                    </dl>
+                </Window>
+            )}
+        </div>
+    );
+};
+export default App;
